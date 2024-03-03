378e024d6a704f010537cd123658891e7234ae53
Your Name <you@example.com>
2024-03-03 21:34:07 +0800

    初步实现复制markdown功能

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -20,7 +20,12 @@
           <div class="content-wrapper" v-if="infoContent" v-html="infoContent"></div>
           <div v-else>No Information</div>
         </div>
-        <button @click="showInfo">Show Info</button>
+
+        <div class="button-container" style="display: flex; justify-content: space-between;">
+          <button @click="showInfo">Show Info</button>
+          <!-- 新增的第二个按钮 -->
+          <button @click="anotherAction">Another Action</button>
+        </div>
       </div>
     </div>
   </div>
@@ -29,6 +34,7 @@
 <script>
 // 在 Vue 组件的 <script> 部分
 import { MathpixMarkdownModel } from 'mathpix-markdown-it';
+import TurndownService from 'turndown';
 export default {
   name: 'App',
   data() {
@@ -63,15 +69,69 @@ export default {
       reader.readAsDataURL(file);
     },
 
+    convertHtmlToMarkdown(html) {
+      const turndownService = new TurndownService();
+
+      turndownService.addRule('tables', {
+        // 这个filter函数用来指定哪些元素会被当前规则处理
+        filter: function (node) {
+          return node.nodeName === 'TABLE';
+        },
+
+        // replacement函数定义了如何将捕获的HTML元素转换为Markdown
+        replacement: function (content, node) {
+          var markdown = '';
+          var rows = node.querySelectorAll('tr');
+
+          // 遍历所有的行 <tr>
+          rows.forEach(function (row, rowIndex) {
+            var cells = row.querySelectorAll('td, th');
+            var rowMarkdown = '|';
+
+            // 遍历所有的单元格 <td> 或 <th>
+            cells.forEach(function (cell) {
+              // 获取单元格的文本内容，并添加到Markdown表格行
+              rowMarkdown += cell.textContent.trim() + '|';
+            });
+
+            // 添加当前行到Markdown表格，并添加一个换行符
+            markdown += rowMarkdown + '\n';
+
+            // 对于第一行，添加Markdown表格头分隔符
+            if (rowIndex === 0) {
+              markdown += '|' + ' --- |'.repeat(cells.length) + '\n';
+            }
+          });
+
+          return markdown;
+        }
+      });
+      return turndownService.turndown(html);
+    },
+    anotherAction() {
+
+      // 调用convertHtmlToMarkdown方法进行转换
+      const markdown = this.convertHtmlToMarkdown(this.infoContent);
+      // 输出转换后的Markdown内容
+      //console.log(markdown);
+      // 使用navigator.clipboard.writeText复制文本到剪贴板
+      navigator.clipboard.writeText(markdown).then(() => {
+        // 复制成功后，你可以在这里执行一些操作，比如显示提示信息
+        ('文本复制成功！');
+      }).catch(err => {
+        // 如果复制失败，你可以在这里处理错误，比如显示错误提示
+        console.error('复制文本失败：', err);
+      });
+    },
+
     showInfo() {
       /* 这里 */
       this.infoContent = "";
 
       // 示例 LaTeX 字符串
       const latexString =
-        `
-   
-  `;
+        `aada
+      `;
       const options = {
       };
       // 将 LaTeX 转换为 HTML
